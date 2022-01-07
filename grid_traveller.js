b659e0424158d42d68f9178f2d9b4056b5654ed6
Manpreet Kaur <64475346+mkaur-1@users.noreply.github.com>
2022-01-07 21:38:05 +0530

    Update grid_traveller.js

--- grid_traveller.js
+++ grid_traveller.js
@@ -1,3 +1,4 @@
+//Recursion
 const gridTraveller=(m,n) =>
 {
 if(m===1 && n===1)
@@ -8,6 +9,25 @@ return gridTraveller(m-1,n)+gridtraveller(m,n-1);
 };
 
 console.log(gridTraveller(1,1));
-console.log(gridTraveller(2,2));
-console.log(gridTraveller(3,3));
+console.log(gridTraveller(2,3));
+console.log(gridTraveller(3,2));
 console.log(gridTravller(18,18));
+
+//Memoization
+const gridTraveller=(m,n,memo={}) =>
+{
+  const key=m+','+n;
+  if(key in memo)
+    return memo[key];
+  if(m===1 && n===1)
+return 1;
+if(m===0 && m===0)
+return 0;
+memo[key]= gridTraveller(m-1,n,memo)+gridtraveller(m,n-1,memo);
+  return memo[key];
+  console.log(gridTraveller(1,1));
+console.log(gridTraveller(2,3));
+console.log(gridTraveller(3,2));
+console.log(gridTravller(18,18));
+
+};
